966990e4b9e963d9ef4cd36a66e6757bfbbfa12e
efrain <efarinva98@gmail.com>
2020-03-31 16:20:29 -0400

    fixed alignment

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ import icon from'./Frame1.png'
 const Nav = () => {
 
   return(<section className='whole'>
-    <a href='/home'> <img src={icon} alt='icon'/></a>
+    <a href='/'> <img src={icon} alt='icon'/></a>
     <div className='right-nav-panel'>
       <a className='nav-word' href='/portfolio'> Portfolio</a>
       <a className='nav-word' href='/'> About Me</a>
