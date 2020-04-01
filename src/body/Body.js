dc8bc02f55ddbb72d1a38e83ff5f19d9e806a722
efrain <efarinva98@gmail.com>
2020-04-01 19:56:55 -0400

    adding a couple pictures and scroll

--- src/body/Body.js
+++ src/body/Body.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 
 import Drawing from '../Anthonydrawing.png'
 import iphone from '../Iphone.png'
+import arrow from '../Vector.png'
 
 import './body.css'
 
 
+const scrollToRef=(el)=>{
+  window.scrollTo(0,el.current.offsetTop)  
+}
+// const scrollToRef = (ref) => window.scrollTo(2000, ref.current)   
 const Body = () => {
+  const theref = React.createRef()
+  useEffect(()=>{scrollToRef(theref)},[])
+
   return(<div className='landing-page'>
   
-    <section className='top-landing-sec'>
+    <section className='top-landing-sec' >
       <div className='name-container'>
         <h1 className='name'>Anthony Holder</h1>
         <p className='tony-text-one'>
@@ -17,11 +25,14 @@ const Body = () => {
           I have a creative and curious mind and a love for art, 
           design, and technology. 
         </p>
+<button onClick={()=>scrollToRef(theref)} >
+        <img className='arrow-pic' src={arrow} alt ='down ward arrow'/>
+  </button> 
       </div>
       <img className='tonys-drawing' src={Drawing} alt='portrait'/>
     </section>
 
-    <a className='bottom-landing-sec' href='/case1'>
+    <a className='bottom-landing-sec' href='/case1'  >
       <div className='left-contain-phone'>
         <h1 className='projects'>Projects</h1>
         <img  className='tony-iphone' src={iphone} alt='iphone-pic'/>
@@ -29,14 +40,13 @@ const Body = () => {
 
       <div className='anywhere-fit'>
         <h2>Anywhere Fitness</h2>
-        <p className='anywhere-p'> 
+        <p className='anywhere-p' ref={theref}> 
           An app to help users 
           find fitness classes hosted by local instructors.
         </p>
         <h3>View Case Studies</h3>
       </div>
     </a>
-
 
   </div>)
 }
--- src/body/Third.js
+++ src/body/Third.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import phone1 from '../HomePage.png'
+import phone2 from '../CategoriespageUnselected.png';
+import phone3 from '../SelectAccountType.png';
+import benjones from '../Proto-Persona-Instructor.png'
+
 import './third.css'
 const Third =()=>{
   return(<>
@@ -14,13 +19,21 @@ const Third =()=>{
       <p>Anywhere Fitness is an app that helps users find fitness classes available in their area. The app will also help local instructors by giving them tools to post and edit their classes.</p>
     </section>
     <section className='anywhere-3'>
-
+      <img src={phone1} alt='phone-display1'/>
+      <img src={phone2} alt='phone-display2'/>
+      <img src={phone3} alt='phone-display3'/>
     </section>
     <section className='how-to-help'>
-      
+      <h2>How to help users?</h2>
+      <p>
+        To get a better insight from the users, I ran several interviews with participants that have attended or currently attend a fitness class and participants that teach fitness classes. This process helped find key pain points.
+      </p>
     </section>
     <section className='ben-jones'>
-
+      <img src={benjones} alt='picture of ben jones'/>
+      <p>
+        I was having a hard time focusing on my research, and There were a lot of issues the users are experiencing and needed to prioritize those issues. To help me with this, I created proto-personas for each user type. The personas were created based on the feedback received from interviews and surveys. It helped me see how each issue can affect the users and possible ways to solve them.
+      </p>
     </section>
     <section className='jorney-map'>
 
