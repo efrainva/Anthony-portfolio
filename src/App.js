2b1dab5e050aeb62e65d0b6eff4c89c4cfaf0108
efrain <efarinva98@gmail.com>
2020-04-08 19:17:45 -0400

    about me name change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import Body from './body/Body';
-import Second from './Second'
+import about from './body/aboutme'
 import Third from './body/Third'
 import {Switch,Route} from 'react-router-dom'
 
@@ -16,7 +16,7 @@ function App() {
       <Nav/>
       <Switch>
         <Route exact path='/' component={Body} />
-        <Route path='/portfolio' component={Second}/>
+        <Route path='/portfolio' component={about}/>
         <Route path='/case1' component={Third} />
       </Switch>
       <Footer/>
---
+++ src/body/aboutme.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import tonypic from './Ellipse1.png'
+// this component uses the body css style sheet
+import './body/body.css'
+
+
+const aboutme =()=>{
+  return(<div className='second'>
+    <img  className='tony-profile' src={tonypic} alt='selfie'/>
+    <section className='second-p'>
+      <h2>I'm Anthony Holder</h2>
+      <p>I grew up with a passion for art, comic books, and technology. 
+      In 2019 I joined the Lambda UX Design program to gain more knowledge and experience.
+      <br/>
+      <br/>
+      My transition from artist to UX designer has been a fun and challenging one. 
+      My new skills and mindset of helping users allows me to create designs that are modern and functional
+      <br/>
+      <br/> In my free time, I enjoy drawing, 
+      listen to music, play video games, 
+      and watch football (soccer).
+      </p>
+      <h3>Contact</h3>
+      <h3>View Work</h3>
+    </section>
+  </div>)
+}
+
+export default aboutme;
